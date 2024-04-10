c510886d83193a6ddb3e79e151834dccd598e0df
njoaco <joaquinlucasnieto@gmail.com>
2024-04-10 16:35:08 -0300

    Perfil page added

--- src/AdminDashboard.java
+++ src/AdminDashboard.java
@@ -138,6 +138,7 @@ public class AdminDashboard extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        new Profile(username).setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
